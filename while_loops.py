a0ac3441281f992c68e4e71d8c0c618a5a94124e
smmundada95 <smmundada95@gmail.com>
2019-02-06 00:05:42 +0530

    Code commits for while_loops.py

---
+++ while_loops.py
@@ -0,0 +1,21 @@
+def get_answer(prompt):
+    '''(str) -> str '''
+
+    answer = input(prompt)
+
+    while not (answer == 'yes' or answer == 'no'):
+        answer = input(prompt)
+
+    return answer
+
+def up_to_vowel(s):
+    '''(str) -> str '''
+
+    before_vowel = ''
+    i = 0
+
+    while i < len(s) and not (s[i] in 'aeiouAEIOU'):
+        before_vowel = before_vowel + s[i]
+        i = i + 1
+
+    return before_vowel
